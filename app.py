dcc086505916fda6d3a409b6f41c4b888dac02f5
Kirill <kiryasp@mail.ru>
2024-09-05 10:44:32 +0300

    ADD FILE

---
+++ app.py
@@ -0,0 +1,5 @@
+def main():
+           print("Hello, World!")
+
+       if name == "main":
+           main()
---
+++ tets.app.py
@@ -0,0 +1,5 @@
+def test_main(capsys):
+           from app import main
+           main()
+           captured = capsys.readouterr()
+           assert captured.out == "Hello, World!\n"
